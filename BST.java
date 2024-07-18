7f1aa3e4133b30f693b423770cb95544e361d18e
V <37008578+crustmaster@users.noreply.github.com>
2024-07-18 16:41:33 +0800

    Update BST.java
    leetcode
    #98 验证搜索二叉树    https://leetcode.cn/problems/validate-binary-search-tree/description/
    #173 二叉搜索树迭代器    https://leetcode.cn/problems/binary-search-tree-iterator/description/
    #700 二叉搜索树中的搜索    https://leetcode.cn/problems/search-in-a-binary-search-tree/description/
    #701 二叉搜索树中的插入操作    https://leetcode.cn/problems/insert-into-a-binary-search-tree/description/
    #450 删除二叉树中的节点    https://leetcode.cn/problems/delete-node-in-a-bst/description/

--- BST.java
+++ BST.java
@@ -55,6 +55,32 @@ public class BST {
         return parent;
     }
 
+    /* delete a node in a BST, and return the new BST. (recursion) */
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) { return null; }
+        if (root.val > key) { root.left = deleteNode(root.left, key); }
+        if (root.val < key) { root.right = deleteNode(root.right, key); }
+        if (root.val == key) {
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            if (root.left == null) {
+                return root.right;
+            }
+            TreeNode successor = root.right;
+            while (successor.left != null) {
+                successor = successor.left;
+            }
+            root.right = deleteNode(root.right, successor.val);
+            successor.left = root.left;
+            successor.right = root.right;
+            return successor;
+        }
+        return root;    
+    }
     
 
 
@@ -74,4 +100,5 @@ leetcode
 #173 二叉搜索树迭代器    https://leetcode.cn/problems/binary-search-tree-iterator/description/
 #700 二叉搜索树中的搜索    https://leetcode.cn/problems/search-in-a-binary-search-tree/description/
 #701 二叉搜索树中的插入操作    https://leetcode.cn/problems/insert-into-a-binary-search-tree/description/
+#450 删除二叉树中的节点    https://leetcode.cn/problems/delete-node-in-a-bst/description/
 */
