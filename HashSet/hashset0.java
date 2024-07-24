146ee4427f674e965e14881b1476366df2cd4932
V <37008578+crustmaster@users.noreply.github.com>
2024-07-24 14:25:29 +0800

    create an array as big as the dataset.

--- HashSet/hashset0.java
+++ HashSet/hashset0.java
@@ -1,1 +1,18 @@
 
+class hashset0 {
+
+    boolean[] judge  = new boolean[1000001];   /* 0 <= key <= 10^6 */
+    
+    public void add(int key) {
+        judge[key] = true;
+    }
+    
+    public void remove(int key) {
+        judge[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return judge[key];
+    }
+}
+
